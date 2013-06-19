47403722a60fe9240f478e736270259e94c2cf2d
dpol <dipolukarov@gmail.com>
2013-06-19 19:50:21 +0400

    Add moving params to user mode task throw registry.

--- asm.h
+++ asm.h
@@ -1,1 +1,5 @@
 void activate(void);
+unsigned int first_stack[256];
+unsigned int *first_stack_start = first_stack + 256 - 16;
+first_stack_start[0] = 0x10;
+first_stack_start[1] = (unsigned int)&first;
--- context_switch.s
+++ context_switch.s
@@ -1,12 +1,11 @@
 .global activate
 activate:
-	mov	r0, #0x10
-	msr	SPSR, r0
-	ldr	lr, =first
+	ldmfd	r0!, {ip,lr}	/* Get SPSR and lr */
+	msr	SPSR, ip
 
-	mov	ip, sp
 	msr	CPSR_c, #0xDF	/* System mode */
-	mov	sp, ip
+	mov	sp, r0
+	pop	{r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,fp,ip,lr}
 	msr	CPSR_c, #0xD3	/* Supervisor mode */
 
 	movs	pc, lr
