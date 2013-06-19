ea4e4e11071d1d0e357b66a7c3f885087d6ebd30
dpol <dipolukarov@gmail.com>
2013-06-19 21:31:18 +0400

    change tasks definitions

--- kernel.c
+++ kernel.c
@@ -18,20 +18,31 @@ void first(void)
 	bwputs("In user mode 1\n");
 	syscall();
 	bwputs("In user mode 2\n");
-	syscall();
+	while (1) syscall();
+}
+
+void task(void)
+{
+	bwputs("In other task\n");
+	while (1) syscall();
 }
 
 int main(void)
 {
-	unsigned int first_stack[256];
-	unsigned int *first_stack_start = first_stack + 256 - 16;
-	first_stack_start[0] = 0x10;
-	first_stack_start[1] = (unsigned int)&first;
+	unsigned int stacks[2][256];
+	unsigned int *tasks[2];
+	tasks[0] = stacks[0] + 256 - 16;
+	tasks[0][0] = 0x10;
+	tasks[0][1] = (unsigned int)&first;
+
+	tasks[1] = stacks[1] + 256 - 16;
+	tasks[1][0] = 0x10;
+	tasks[1][1] = (unsigned int)&task;
 
 	bwputs("Starting!\n");
-	first_stack_start = activate(first_stack_start);
+	tasks[0] = activate(tasks[0]);
 	bwputs("Heading back to user mode\n");
-	first_stack_start = activate(first_stack_start);
+	tasks[1] = activate(tasks[1]);
 	bwputs("Done\n");
 
 	while (1); /* We can't exit, there's nowhere to go */
