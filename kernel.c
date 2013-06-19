f3233a26ca343a3ee5d2a255c66265775d36acbd
dpol <dipolukarov@gmail.com>
2013-06-19 20:06:15 +0400

    fix

--- kernel.c
+++ kernel.c
@@ -13,15 +13,22 @@ void bwputs(char *s)
 	}
 }
 
-int main(void)
-{
-	bwputs("Hello, World!\n");
-
-	return 0;
-}
-
 void first(void)
 {
 	bwputs("In user mode\n");
 	while(1);
+}
+
+int main(void)
+{
+	unsigned int first_stack[256];
+	unsigned int *first_stack_start = first_stack + 256 - 16;
+	first_stack_start[0] = 0x10;
+	first_stack_start[1] = (unsigned int)&first;
+
+	bwputs("Starting!\n");
+	activate(first_stack_start);
+
+	while (1); /* We can't exit, there's nowhere to go */
+	return 0;
 }
