d873645b41bda1702d336d84b448fd2cbe630806
dpol <dipolukarov@gmail.com>
2013-06-19 19:15:27 +0400

    Put hardware specific to header file. Create puts function.

--- kernel.c
+++ kernel.c
@@ -1,16 +1,20 @@
 /**
  *
  */
+#include "versatilepb.h"
+
+void bwputs(char *s)
+{
+	while (*s) {
+		while (*(UART0 + UARTFR) & UARTFR_TXFF);
+		*UART0 = *s;
+		s++;
+	}
+}
 
 int main(void)
 {
-	char *string = "Hello, World!\n";
-	while (*string) {
-		*(volatile char *)0x101f1000 = *string;
-		string++;
-	}
-
-	while (1); /* We can't exit, there's nowhere to go */
+	bwputs("Hello, World!\n");
 
 	return 0;
 }
